cc0a7f8fbd16a9a29e6b84f45b699c0f5d396e01
Md Rabbi <mdrabbi638383@gmail.com>
2025-07-02 00:07:20 +0600

    Fixed Footer link and container margin

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ menuIcon.addEventListener('click',()=>{
         mobileMenu.style.display = 'block';
     }
 })
+document.addEventListener('DOMContentLoaded',function(){
+    const links=document.querySelectorAll('.footer-link');
+    links.forEach(function(link){
+        link.addEventListener('click',function(event){
+            event.preventDefault();
+            console.log("link.clicked:",link.textContent);
+        })
+    })
+})
+
+
